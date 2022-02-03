0d456a008f517b0a76d1ce5327e84f83a42e12bf
kemalbekcan <kemalbekcan@gmail.com>
2022-02-04 00:15:57 +0300

    Added Coding Challange `sumOfDigits`

--- Coding Challenge/sumOfDigits.js
+++ Coding Challenge/sumOfDigits.js
@@ -1,16 +1,12 @@
 function digital_root(n) {
-    // ...
-    console.log("n", n)
     n = n.toString()
     var total = 0
-    for(var i = 0; i <= parseInt(n) && total < 10; i++){
+    for(var i = 0; i < n.length; i++){
         if(n[i] !== undefined){
-            console.log("n", n[i])
             total += Number(n[i])
         }
     }
-    console.log("total", total)
-    return total > 10 ? digital_root(total) : total
+    return total >= 10 ? digital_root(total) :  console.log(total)
 }
 
 digital_root(15)
