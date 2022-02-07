bd58692ee3bad008c7032aa3df53559db6759275
kemalbekcan <kemalbekcan@gmail.com>
2022-02-08 00:33:29 +0300

    Added coding challange `Who Like It`

--- Coding Challenge/whoLikesIt.js
+++ Coding Challenge/whoLikesIt.js
@@ -1,25 +1,14 @@
 function likes(names) {
   // TODO
-  var count = names.length;
-  var absolute = " likes this";
-  var sonuc = "";
-  for (var i = 0; i < names.length; i++) {
-    sonuc += names[i];
+  if (names.length === 0) {
+    return "no one likes this";
+  } else if (names.length === 1) {
+    return `${names[0]} likes this`;
+  } else if (names.length === 2) {
+    return `${names[0]} and ${names[1]} like this`;
+  } else if (names.length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  } else if (names.length > 3) {
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
   }
-  sonuc = names.length === 1 ? names.join("") : names.join(" and ");
-  if (names.length === 3) {
-    names.splice(names.length - 1, 0, "and");
-    sonuc = names.join(" ");
-    sonuc = sonuc.replace(names[0], names[0] + ",");
-  } else if (names.length === 4) {
-    names.pop();
-    names.pop();
-    names.splice(names.length, 0, "and");
-    sonuc = names.join(" ");
-    sonuc = sonuc.replace(names[0], names[0] + ",");
-    sonuc = sonuc + ` ${count - 2} others`;
-  }
-  return names.length == 0
-    ? "no one likes this"
-    : sonuc + ` ${names.length > 1 ? "like" : "likes"} this`;
 }
